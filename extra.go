4ae53336960915bbee0418d14d16ecc4266caaab
mengqi <5b5f7426@gmail.com>
2019-12-20 15:46:21 +0800

    add ParseHeader()

--- extra.go
+++ extra.go
@@ -1,6 +1,9 @@
 package parsemail
 
-import "net/mail"
+import (
+    "io"
+    "net/mail"
+)
 
 
 func (email *Email) RealSender() *mail.Address {
@@ -14,4 +17,20 @@ func (email *Email) RealSender() *mail.Address {
 
     return email.From[0]
 }
+
+
+func ParseHeader(r io.Reader) (email Email, err error) {
+    msg, err := mail.ReadMessage(r)
+    if err != nil {
+        return
+    }
+
+    email, err = createEmailFromHeader(msg.Header)
+    if err != nil {
+        return
+    }
+
+    return
+}
+
 
